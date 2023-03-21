1d80dc3ef145a9fe3d2a6f1836135422aab4a12f
ameen-ansari <ameen55668@gmail.com>
2023-03-21 13:36:25 +0500

    add iframe

--- components/MovieSec/MovieSec.jsx
+++ components/MovieSec/MovieSec.jsx
@@ -3,6 +3,8 @@ import img from "/Images/Header/BG (3).svg";
 import style from "@/styles/LandingPage/MovieSec.module.css";
 
 function MovieSec() {
+
+  let src = "https://youtu.be/vClv4Q2hSgo"
   return (
     <div className={style.parent}>
       <div>
@@ -13,8 +15,13 @@ function MovieSec() {
         </p>
       </div>
       <div>
-        <iframe src="https://youtu.be/embed/vClv4Q2hSgo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen >
-        </iframe>
+      <iframe
+          src="https://www.youtube-nocookie.com/embed/ToZSFHUJdHM"
+          title="Video"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
       </div>
     </div>
   );
